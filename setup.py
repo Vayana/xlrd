6d7f41be63e357844d7ec0b08e4478cccd64000e
Vineet Mago <vineet@vayana.in>
2012-08-21 15:15:29 +0530

    Update build version
    also, ignore build artifacts and swap files

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def mkargs(**kwargs):
 
 args = mkargs(
     name = 'xlrd',
-    version = '0.7.1',
+    version = '0.7.1vayana',
     author = 'John Machin',
     author_email = 'sjmachin@lexicon.net',
     url = the_url,
